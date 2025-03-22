919e430f8f3f4365aafff36451c6244baab17429
Kerem Türközü <turkozukerem@gmail.com>
2025-03-22 20:10:41 +0300

    Initial Commit

---
+++ BluetoothDeviceFinder/BluetoothDeviceFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothDeviceFinderApp.swift
+//  BluetoothDeviceFinder
+//
+//  Created by Kerem Türközü on 22.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothDeviceFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothDeviceFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BluetoothDeviceFinder
+//
+//  Created by Kerem Türközü on 22.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
